ac5ffaaa0eae4f505c10e435c07ed1b670a87017
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-09 18:13:25 -0300

    Add initial implementation of text analyser

--- textanalysis/handler.js
+++ textanalysis/handler.js
@@ -1,6 +1,13 @@
 "use strict"
 
+let now = require('nano-time');
+var Sentiment = require('sentiment');
 
 module.exports = function(req) {
-    return 'boo'
+    var before = now();
+    var sentiment = new Sentiment();
+    console.log(process.env.text_to_analysis);
+    sentiment.analyze(process.env.text_to_analysis);
+    var after = now();
+    return (after - before).toString();
 }
